67d4976d139ed9909c070eb5d59f790ebba13d56
Antonio-cod <antonio_carlostads@hotmail.com>
2023-11-28 20:02:03 -0400

    Corrigindo Projeto

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    formats:['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '',
-        port:'',
-        pathname:'**',
-      },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     formats:['image/avif', 'image/webp'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '',
+//         port:'',
+//         pathname:'**',
+//       },
 
-    ],
-  },
-};
+//     ],
+//   },
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig
